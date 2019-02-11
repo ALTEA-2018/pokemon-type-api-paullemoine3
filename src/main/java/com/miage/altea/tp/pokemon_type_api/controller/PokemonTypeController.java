b3f7cd2eda28af31dad831fe3ed0fdecd3de4265
Paul Lemoine <paul.lemoine.1@gmail.com>
2019-02-11 11:57:55 +0100

    add sort for getter of all pokemons

--- src/main/java/com/miage/altea/tp/pokemon_type_api/controller/PokemonTypeController.java
+++ src/main/java/com/miage/altea/tp/pokemon_type_api/controller/PokemonTypeController.java
@@ -9,6 +9,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+import java.util.Comparator;
 import javax.websocket.server.PathParam;
 import java.util.List;
 
@@ -29,7 +31,9 @@ public class PokemonTypeController {
     }
     @GetMapping("/")
     public List<PokemonType> getAllPokemonTypes() {
-        return pokemonTypeService.getAllPokemonTypes();
+        List<PokemonType> listPT = pokemonTypeService.getAllPokemonTypes();
+        Collections.sort(listPT, (left, right) -> left.getId() - right.getId());
+        return listPT;
     }
 
     @GetMapping(value ="/", params="name")
